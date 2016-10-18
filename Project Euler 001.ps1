0242fc84c537e990f9682fb1665192470b832b70
Stanley Brown <stanley.brown@concur.com>
2016-10-18 13:04:40 -0400

    Added Solution to problem 1

---
+++ Project Euler 001.ps1
@@ -0,0 +1,16 @@
+﻿$Sum=0
+
+
+For ($i=0; $i -lt 1000; $i++) {
+    $Answer1 = $i%3
+    $Answer2 = $i%5
+    #Write-Host "$i%3 = $Answer1"
+    #Write-Host "$i%5 = $Answer2"
+
+    if(($Answer1 -eq 0) -or ($Answer2 -eq 0))
+     {
+        $Sum = $Sum + $i
+        Write-Host "Sum = $Sum through i = $i"
+        }
+    }
+
